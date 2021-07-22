d6250fe32fa7f08fe59a5d567456bc4c578147b9
BlackEyes27920 <tranquangduc000@gmail.com>
2021-07-23 00:37:03 +0700

    Commit Menu

--- src/components/common/NavLeft.vue
+++ src/components/common/NavLeft.vue
@@ -1,7 +1,24 @@
 <template>
   <div ref="navLeft" :class="isOpenNavLeft ? 'open' : 'hidden'">
     <v-scroll class="ps">
-      <div class="hp-left__container">Hello World</div>
+      <div class="hp-left__container py-3">
+        <div class="hp-left__logo mb-5">
+          <v-svg :src="require('@/assets/icons/Travigo.svg')" />
+        </div>
+
+        <a
+          href="#"
+          v-for="(o, i) of menuLeft"
+          :key="i"
+          :class="{ active: o.active }"
+          class="hp-left__nav-item mx-3 mx-xxl-4"
+          v-ripple="{ color: o.active ? 'currentColor' : 'var(--primary)' }"
+        >
+          <!-- :to="{ name: o.router }" -->
+          <v-svg :src="o.active ? o.iconActive : o.icon" />
+          <span>{{ o.label }}</span>
+        </a>
+      </div>
     </v-scroll>
   </div>
 </template>
@@ -10,6 +27,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Vue, Component } from "vue-property-decorator";
 import { namespace } from "vuex-class";
+import { menuLeft } from "@/config";
 
 const homePage = namespace("homePage");
 
@@ -17,7 +35,7 @@ const homePage = namespace("homePage");
 export default class App extends Vue {
   @homePage.State("isOpenNavLeft") isOpenNavLeft!: boolean;
   @homePage.Mutation("toggleIsOpenNavLeft") toggleIsOpenNavLeft!: any;
+
+  menuLeft = menuLeft;
 }
 </script>
-
-<style lang="less" scoped></style>
---
+++ src/config.ts
@@ -0,0 +1,33 @@
+export const menuLeft = [
+  {
+    label: "Darkboard",
+    router: "Darkboard",
+    icon: require("@/assets/icons/home-regular.svg"),
+    iconActive: require("@/assets/icons/home-solid.svg"),
+    active: true,
+  },
+  {
+    label: "My Tickets",
+    router: "Tickets",
+    icon: require("@/assets/icons/home-regular.svg"),
+    iconActive: require("@/assets/icons/home-solid.svg"),
+  },
+  {
+    label: "Favorite",
+    router: "Favorite",
+    icon: require("@/assets/icons/home-regular.svg"),
+    iconActive: require("@/assets/icons/home-solid.svg"),
+  },
+  {
+    label: "Transaction",
+    router: "Transaction",
+    icon: require("@/assets/icons/home-regular.svg"),
+    iconActive: require("@/assets/icons/home-solid.svg"),
+  },
+  {
+    label: "Settings",
+    router: "Settings",
+    icon: require("@/assets/icons/home-regular.svg"),
+    iconActive: require("@/assets/icons/home-solid.svg"),
+  },
+];
